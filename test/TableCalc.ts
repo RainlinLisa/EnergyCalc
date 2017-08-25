2558f0db3207c87dfa7a257c7ba8af80e8b3b3df
Rainlin <rlinnyaka@gmail.com>
2017-08-25 20:20:19 +0300

    (Fix) tslint config and fix, настроен линтер и исправлены ошибки

--- test/TableCalc.ts
+++ test/TableCalc.ts
@@ -3,80 +3,80 @@ import Component from "vue-class-component";
 
 @Component({
 	props: [
-		'template'
+		"template"
 	]
 })
-export default class TableCalc extends Vue {	
+export default class TableCalc extends Vue {
 	private EnergyYearArray = [];
 	private numberReplaceArray = [];
 	private costNumberReplaceArray = [];
 	private allCostYearArray = [];
 	private savingYearArray = [];
-	
+
 	get computedAll () {
-		return (this.$store.getters.costEnergy && this.$store.getters.hrYear && this.$store.getters.quantityLamp)
+		return (this.$store.getters.costEnergy && this.$store.getters.hrYear && this.$store.getters.quantityLamp);
 	}
 
     get lifeTime () {
         return [this.$store.getters.lifeTimeFilam, this.$store.getters.lifeTimeLum, this.$store.getters.lifeTimeSvet];
     }
-	
+
 	get costLamp () {
         return [this.$store.getters.costLampFilam, this.$store.getters.costLampLum, this.$store.getters.costLampSvet];
     }
-	
+
 	// Стоимость электроэнергии за 1 год, руб.
 	get EnergyYear () {
-		this.EnergyYearArray = [0,0,0];
+		this.EnergyYearArray = [0, 0, 0];
 		if (this.computedAll) {
-			for (let i=0; i < this.$store.getters.powerYear.length; i++) {
-				this.EnergyYearArray[i] = (this.$store.getters.powerYear[i] * this.$store.getters.costEnergy)
+			for (let i = 0; i < this.$store.getters.powerYear.length; i++) {
+				this.EnergyYearArray[i] = (this.$store.getters.powerYear[i] * this.$store.getters.costEnergy);
 			}
 		}
 		return this.EnergyYearArray;
     }
-	
+
 	// Количество ламп под замену или условное количество ламп с выработанным ресурсом за 1 год, шт.
 	get numberReplaceLamp () {
-		this.numberReplaceArray = [0,0,0];
+		this.numberReplaceArray = [0, 0, 0];
 		if (this.computedAll) {
-			for (let i=0; i < this.lifeTime.length; i++) {
+			for (let i = 0; i < this.lifeTime.length; i++) {
 				this.numberReplaceArray[i] = ((this.$store.getters.hrYear * this.$store.getters.quantityLamp) / this.lifeTime[i] );
 			}
 		}
 		return this.numberReplaceArray;
     }
-	
+
 	// Стоимость ламп под замену за 1 год (накопления на замену ламп), руб
 	get costNumberReplaceYear () {
-		this.costNumberReplaceArray = [0,0,0];
+		this.costNumberReplaceArray = [0, 0, 0];
 		if (this.computedAll) {
-			for (let i=0; i < this.costLamp.length; i++) {
+			for (let i = 0; i < this.costLamp.length; i++) {
 				this.costNumberReplaceArray[i] = (this.numberReplaceLamp[i] * this.costLamp[i]);
 			}
 		}
 		return this.costNumberReplaceArray;
     }
-	
+
 	// Всего затрат на 1 год, руб.
 	get allCostYear () {
-		this.allCostYearArray = [0,0,0];
+		this.allCostYearArray = [0, 0, 0];
 		if (this.computedAll) {
-			for (let i=0; i < this.EnergyYear.length; i++) {
+			for (let i = 0; i < this.EnergyYear.length; i++) {
 				this.allCostYearArray[i] = (this.EnergyYear[i] + this.costNumberReplaceYear[i]);
 			}
 		}
 		return this.allCostYearArray;
     }
-	
+
 	// СБЕРЕЖЕНИЯ за N год (по сравнению с лампами накаливания), руб.
 	get savingYear () {
-		this.savingYearArray = [0,0,0];
+		this.savingYearArray = [0, 0, 0];
 		if (this.computedAll) {
-			for (let i=0; i < this.EnergyYear.length; i++) {
+			for (let i = 0; i < this.EnergyYear.length; i++) {
 				this.savingYearArray[i] = (this.allCostYear[0] - this.allCostYear[i]);
 			}
 		}
 		return this.savingYearArray;
-    }  
+    }
 }
--- test/main.ts
+++ test/main.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Component from "vue-class-component"
+import Component from "vue-class-component";
 import CalcProperty from "./calc_energy.vue";
 import TableCalc from "./tableCalcComp.vue";
 import Vuex from "vuex";
@@ -8,16 +8,16 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    quantityLamp: '',
-	hrYear: '',
-	costEnergy: '',
-	lifeTimeFilam: '1000.00',
-	lifeTimeLum: '8000.00',
-	lifeTimeSvet: '20000.00',
-	powerYear: [0,0,0],
-	costLampFilam: '20.00',
-	costLampLum: '90.00',
-	costLampSvet: '120.00',
+    quantityLamp: "",
+	hrYear: "",
+	costEnergy: "",
+	lifeTimeFilam: "1000.00",
+	lifeTimeLum: "8000.00",
+	lifeTimeSvet: "20000.00",
+	powerYear: [0, 0, 0],
+	costLampFilam: "20.00",
+	costLampLum: "90.00",
+	costLampSvet: "120.00",
   },
   mutations: {
       storeInputs (state, obj) {
@@ -37,44 +37,44 @@ const store = new Vuex.Store({
     quantityLamp (state) {
         return state.quantityLamp;
     },
-	  
+
     hrYear (state) {
         return state.hrYear;
     },
-	  
+
 	lifeTimeFilam (state) {
         return state.lifeTimeFilam;
     },
-	  
+
 	lifeTimeLum (state) {
         return state.lifeTimeLum;
     },
-	  
+
 	lifeTimeSvet (state) {
         return state.lifeTimeSvet;
     },
-	  
+
 	powerYear (state) {
         return state.powerYear;
     },
-	
+
 	costEnergy (state) {
         return state.costEnergy;
     },
-	
+
 	costLampFilam (state) {
         return state.costLampFilam;
     },
-	
+
 	costLampLum (state) {
         return state.costLampLum;
     },
-	
+
 	costLampSvet (state) {
         return state.costLampSvet;
     }
   }
-})
+});
 
 new Vue({
    el: ".test-container",
@@ -84,4 +84,3 @@ new Vue({
        "tablecalc-component": TableCalc,
    }
 });
-
