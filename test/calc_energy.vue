d4ff0b197eec33fa3ca938f40225fb44d47cbb1b
RainlinLisa <rlinnyaka@gmail.com>
2017-09-07 16:11:34 +0300

    работа со стилями

--- test/calc_energy.vue
+++ test/calc_energy.vue
@@ -1,153 +1,167 @@
 <template>
-	<div>
-		<div class="container">
-		<h1>Энергосбережение</h1>
-		</div>	
-		 
-		<div class="container form-group">
-			<form>
-				<div class="control-group controls ">		
-					<div class="row">
-					   <div class="span4">Количество ламп, шт.</div>
-					   <div class="span2">
-							<input 
-								class="form-control input-small text-right" type="text" 
-								v-model.number="quantityLamp" 
-								v-on:keypress="onlyInteger">
-					   </div>
-					</div>
-					<div class="row"> 
-					   <div class="span4">Количество часов работы ламп в год</div>
-					   <div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model="hrYear" 
-								v-on:keypress="onlyInteger">
-						</div>
-					</div>
-					   
-					<div class="row">
-					   <div class="span4">Стоимость электроэнергии - 1 кВт*ч, руб.</div>
-					   <div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="costEnergy" 
-								v-on:keypress="onlyFloat">
-						</div>	
-					</div>
-				</div>
-			</form>
-		</div>
+	<div class="container">
+		<div class="span12">
+		<table class="table">
+		<tbody>
+				<tr>
+				   <td class="span6">Количество ламп, шт.</td>
+				   <td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="quantityLamp" 
+							v-on:keypress="onlyInteger">
+				   </td>
+				   <td class="span2"></td>
+				   <td class="span2"></td>
+				</tr>
+				<tr> 
+				   <td class="span6">Количество часов работы ламп в год</td>
+				   <td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model="hrYear" 
+							v-on:keypress="onlyInteger">
+					</td> 
+				</tr> 
+				<tr>
+				   <td class="span6">Стоимость электроэнергии - 1 кВт*ч, руб.</td>
+				   <td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="costEnergy" 
+							v-on:keypress="onlyFloat">
+					</td>	
+				</tr>
+
 	 
-		<div class="container form-group">
-			<form>
-				<div class="control-group controls">
-					<div class="row">
-						<div class="span4">(Тип лампы)</div>
-						<div class="span2">Лампа накаливания {{ selectedPowerFilam }}W</div>
-						<div class="span2">Лампа компактная люминесцентная {{ selectedPowerLum }}W</div>
-						<div class="span2">Лампа светодиодная {{ selectedPowerSvet }}W</div>
-					</div>
-					
-					<div class="row">
-						<div class="span4">Мощность лампы, Вт</div>
-						<div class="span2">
-							<select class="input-small" v-model="selectedPowerFilam">
+
+				<tr>
+					<td class="span6"></td>
+					<td class="span2 shift">
+						<div class="filam"></div>
+						<div> Лампа накаливания {{ selectedPowerFilam }}W</div>						
+					</td>				
+					<td class="span2 shift">			
+						<div class="lumin"></div>
+						<div> Лампа компактная люминесцентная {{ selectedPowerLum }}W</div>							
+					</td>					
+					<td class="span2 shift">						
+						<div class="led"></div>
+						<div> Лампа светодиодная {{ selectedPowerSvet }}W</div>							
+					</td>
+				</tr>
+				
+				<tr>
+					<td class="span6">Мощность лампы, Вт</td>
+					<td class="span2">
+						<select class="span2" v-model="selectedPowerFilam">
 							<option v-for="option in optionsFilam" v-bind:value="option.value">
 								{{ option.text }}
 							</option>
-							</select>
-						</div>
-						<div class="span2">
-							<select class="input-small" v-model="selectedPowerLum">
+						</select>
+					</td>
+					<td class="span2">
+						<select class="span2" v-model="selectedPowerLum">
 							<option v-for="option in optionsLum" v-bind:value="option.value">
 								{{ option.text }}
 							</option>
-							</select>
-						</div>
-						<div class="span2">
-							<select class="input-small" v-model="selectedPowerSvet">
+						</select>
+					</td>
+					<td class="span2">
+						<select class="span2" v-model="selectedPowerSvet">
 							<option v-for="option in optionsSvet" v-bind:value="option.value">
 								{{ option.text }}
 							</option>
-							</select>
-						</div>
-					</div>
+						</select>
+					</td>
+				</tr>
+				
+				<tr>
+					<td class="span6">Суммарная мощность ламп, Вт</td>
+					<td class="span2" v-for="item in sumPowerLamp">
+						<input 
+							class="span2 text-right" 
+							style="font-weight: bold" type="text" 
+							disabled v-bind:value="item">
+					</td>
+				</tr>
+				
+				<tr>
+					<td class="span6">Потребляемая электроэнергия за год, кВт*час</td>
+					<td class="span2" v-for="item in consumedEnergyYear">
+						<input 
+							class="span2 text-right" 
+							style="font-weight: bold" type="text" 
+							disabled v-bind:value="item">
+					</td>
+				</tr>
+				
+				<tr>
+					<td class="span6">Срок службы ламп, часов</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="lifeTimeFilam" 
+							v-on:keypress="onlyFloat">
+					</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="lifeTimeLum" 
+							v-on:keypress="onlyFloat">
+					</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="lifeTimeSvet" 
+							v-on:keypress="onlyFloat">
+					</td>
+				<tr>
+				
+				<tr>
+					<td class="span6">Цена за лампу, руб.</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="costLampFilam" 
+							v-on:keypress="onlyFloat">
+					</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="costLampLum" 
+							v-on:keypress="onlyFloat">
+					</td>
+					<td class="span2">
+						<input 
+							class="span2 text-right" type="text" 
+							v-model.number="costLampSvet" 
+							v-on:keypress="onlyFloat">
+					</td>
+				</tr>
+				
+				<tr>
+					<td class="span6" style="font-weight: bold">Первоначальные инвестиции на покупку ламп, руб.</td>
+					<td class="span2" v-for="item in primaryInvest">
+						<input 
+							class="span2 text-right" 
+							style="font-weight: bold" type="text" 
+							disabled v-bind:value="item">
+					</td>	
+				</tr>
+		
 					
-					<div class="row">
-						<div class="span4">Суммарная мощность ламп, Вт</div>
-						<div class="span2" v-for="item in sumPowerLamp">
-							<input class="input-small text-right" type="text" disabled v-bind:value="item">
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="span4">Потребляемая электроэнергия за год, кВт*час</div>
-						<div class="span2" v-for="item in consumedEnergyYear">
-							<input class="input-small text-right" type="text" disabled v-bind:value="item">
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="span4">Срок службы ламп, часов</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="lifeTimeFilam" 
-								v-on:keypress="onlyFloat">
-						</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="lifeTimeLum" 
-								v-on:keypress="onlyFloat">
-						</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="lifeTimeSvet" 
-								v-on:keypress="onlyFloat">
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="span4">Цена за лампу, руб.</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="costLampFilam" 
-								v-on:keypress="onlyFloat">
-						</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="costLampLum" 
-								v-on:keypress="onlyFloat">
-						</div>
-						<div class="span2">
-							<input 
-								class="input-small text-right" type="text" 
-								v-model.number="costLampSvet" 
-								v-on:keypress="onlyFloat">
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="span4">Первоначальные инвестиции на покупку ламп, руб.</div>
-						<div class="span2" v-for="item in primaryInvest">
-							<input 
-								class="input-small text-right" type="text" 
-								disabled v-bind:value="item">
-						</div>	
-					</div>
-				</div>
-			</form>
+		</tbody>
+		</table>
 		</div>
+		
 		<div v-for="item in year">
 			<tablecalc-component v-bind:template="item"></tablecalc-component>
 		</div>
+		
 	</div>
 </template>
+
 
 <script lang="ts">
     import CalcProperty from "./CalcProperty";
--- test/tableCalcComp.vue
+++ test/tableCalcComp.vue
@@ -1,30 +1,29 @@
 <template>
 	<div class="container">
-		<div class="span9">
+		<div class="span12">
 			<table class="table table-bordered table-striped">
 				<thead style="background-image: linear-gradient(0deg, #FEF2EA, #FFFFFF)"><th colspan="4">{{ template }} использования</th></thead>
 				<tbody>
 					<tr>
-						<td class="span4">Стоимость электоэнергии за {{ template }}, руб.</td>
+						<td class="span6">Стоимость электроэнергии за {{ template }}, руб.</td>
 						<td class="span2" v-for="item in costEnergyYear">{{ (item  * template.charAt(0)).toFixed(2)}}</td>
 					</tr>
-					 
 					<tr>
-						<td class="span4">Количество ламп под замену или условное количество ламп с 
+						<td class="span6">Количество ламп под замену или условное количество ламп с 
 							выработанным ресурсом за {{ template }}, шт.</td>
 						<td class="span2" v-for="item in numberReplaceLamp">{{ Math.round(item * template.charAt(0)) }}</td>
 					</tr>
 					<tr>
-						<td class="span4">Стоимость ламп под замену за {{ template }} (накопленная на замену ламп), руб.</td>
+						<td class="span6">Стоимость ламп под замену за {{ template }} (накопленная на замену ламп), руб.</td>
 						<td class="span2" v-for="item in costNumberReplaceYear">{{ (item * template.charAt(0)).toFixed(2) }}</td>
 					</tr>
 					<tr>
-						<td class="span4">Всего затрат за {{ template }}, руб.</td>
+						<td class="span6">Всего затрат за {{ template }}, руб.</td>
 						<td class="span2" v-for="item in allCostYear">{{ (item * template.charAt(0)).toFixed(2) }}</td>
 					</tr>
 					<tr>
-						<td class="span4">СБЕРЕЖЕНИЯ ЗА {{ template.toUpperCase() }} (по сравнению с лампами накаливания), руб.</td>
-						<td class="span2" style = "font-weight: bold" v-for="item in savingYear">{{ (item * template.charAt(0)).toFixed(2) }}</td>
+						<td class="span6" style="font-weight: bold">СБЕРЕЖЕНИЯ ЗА {{ template.toUpperCase() }} (по сравнению с лампами накаливания), руб.</td>
+						<td class="span2" style="font-weight: bold" v-for="item in savingYear">{{ (item * template.charAt(0)).toFixed(2) }}</td>
 					</tr>
 				</tbody>
 			</table>
